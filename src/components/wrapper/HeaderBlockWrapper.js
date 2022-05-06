c1225736e8bb67d39f291bbcedb0cff0dbac752b
Paul Sohns <paul.sohns@web.de>
2022-05-06 23:00:47 +0200

    Continue styling the header text

--- src/components/wrapper/HeaderBlockWrapper.js
+++ src/components/wrapper/HeaderBlockWrapper.js
@@ -11,7 +11,7 @@ function HeaderBlockWrapper() {
 		<div className={styles.headerWrapper}>
 			<div className={styles.leftText}>
 				<div className={styles.headerText}>
-					<h1>Site header here</h1>
+					<h1>Paul Sohns<br/>Portfolio</h1>
 				</div>
 				<div className={styles.aboutMeText}>
 					<p>About me text here Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.</p>
