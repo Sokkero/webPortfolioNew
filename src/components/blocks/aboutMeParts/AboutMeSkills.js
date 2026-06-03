39a52aa0f876480906b0f1a11da733cf2bf63912
Paul Sohns <paul.sohns@web.de>
2026-06-03 16:54:33 +0200

    Add skill tooltips

--- src/components/blocks/aboutMeParts/AboutMeSkills.js
+++ src/components/blocks/aboutMeParts/AboutMeSkills.js
@@ -2,45 +2,44 @@ import React from 'react';
 import styles from '../../../resources/scss/components/blocks/aboutMeParts/aboutMeSkills.module.scss';
 import {AboutMeContent} from "../../../resources/content";
 
+function SkillEntry({entry}) {
+	return (
+		<div className={styles.entry}>
+			<span className={styles.skillName}>
+				{entry.skillName}
+				{entry.hint && (
+					<span className={styles.hint} role="tooltip">{entry.hint}</span>
+				)}
+			</span>
+			<span className={styles.skillMeter}>
+				<span
+					className={styles.skillBar}
+					style={{width: entry.percentage}}
+				/>
+				<span
+					className={styles.skillBox}
+					style={{right: `calc((100% - ${entry.percentage}) - 23px)`}}
+				>{entry.percentage}</span>
+			</span>
+		</div>
+	);
+}
+
 function AboutMeSkills() {
 	return (
 		<div className={styles.mySkills}>
 			<h2>{AboutMeContent.mySkillsContent.headline}</h2>
 
 			{AboutMeContent.mySkillsContent.skillEntries.map((entry, index) => (
-				<div className={styles.entry} key={index}>
-					<span className={styles.skillName}>{entry.skillName}</span>
-					<span className={styles.skillMeter}>
-						<span
-							className={styles.skillBar}
-							style={{width: entry.percentage}}
-						/>
-						<span
-							className={styles.skillBox}
-							style={{right: `calc((100% - ${entry.percentage}) - 23px)`}}
-						>{entry.percentage}</span>
-					</span>
-				</div>
+				<SkillEntry entry={entry} key={index}/>
 			))}
 
 			<span className={styles.seperator}/>
-			
+
 			<h2>{AboutMeContent.myLanguagesContent.headline}</h2>
 
 			{AboutMeContent.myLanguagesContent.languageEntries.map((entry, index) => (
-				<div className={styles.entry} key={index}>
-					<span className={styles.skillName}>{entry.skillName}</span>
-					<span className={styles.skillMeter}>
-						<span
-							className={styles.skillBar}
-							style={{width: entry.percentage}}
-						/>
-						<span
-							className={styles.skillBox}
-							style={{right: `calc((100% - ${entry.percentage}) - 23px)`}}
-						>{entry.percentage}</span>
-					</span>
-				</div>
+				<SkillEntry entry={entry} key={index}/>
 			))}
 		</div>
 	);
